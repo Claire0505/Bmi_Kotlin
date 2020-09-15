6bdd4596ba0cf7a55103640060dc2a06a74efe20
Claire <claire0505@gmail.com>
2020-09-15 17:22:34 +0800

    1.多國語言 2.layout, find views, onClick 3.Toast.make, AlertDialog.Builder 4.EditText判斷使用者是否有輸入值

--- app/src/main/java/com/claire/bmi/MainActivity.kt
+++ app/src/main/java/com/claire/bmi/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +13,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btn_help.setOnClickListener{
+            AlertDialog.Builder(this)
+                .setTitle("BMI說明")
+                .setMessage("體重(kg) / 身高的平方(m)")
+                .setPositiveButton("Ok", null)
+                .show()
+
+        }
+
     }
 
     fun calculateBmi( view : View){
-        val weight = ed_Weight.text.toString().toFloat()
-        val height = ed_Height.text.toString().toFloat()
-        val bmi = weight / (height * height)
-        Log.d("bmi", bmi.toString())
+
+        if (ed_Weight.text.trim().isEmpty() || ed_Height.text.trim().isEmpty()){
+
+            Toast.makeText(this, "Please input your Weight and Height", Toast.LENGTH_SHORT).show()
+
+        } else {
+
+            val weight = ed_Weight.text.toString().toFloat()
+            val height = ed_Height.text.toString().toFloat()
+            val bmi = weight / (height * height)
+
+            Log.d("bmi", bmi.toString())
+            Toast.makeText(this, bmi.toString(),Toast.LENGTH_SHORT).show()
+
+            AlertDialog.Builder(this)
+                .setTitle("Your BMI")
+                .setMessage(bmi.toString())
+                .setPositiveButton("Ok", null)
+                .setNegativeButton("Cancel",  null)
+                .show()
+        }
 
     }
 }
