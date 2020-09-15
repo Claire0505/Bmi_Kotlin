549ecbd110475dd51a3abdee481872ee30bf26a4
Claire <claire0505@gmail.com>
2020-09-15 10:57:25 +0800

    linearLayout onClick

--- app/src/main/java/com/claire/bmi/MainActivity.kt
+++ app/src/main/java/com/claire/bmi/MainActivity.kt
@@ -2,10 +2,22 @@ package com.claire.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun calculateBmi( view : View){
+        val weight = ed_Weight.text.toString().toFloat()
+        val height = ed_Height.text.toString().toFloat()
+        val bmi = weight / (height * height)
+        Log.d("bmi", bmi.toString())
+
     }
 }
